10dbf7c5bdffd5901c348688b8de8f98bf5020bf
Kent Boogaart <kent.boogaart@gmail.com>
2018-04-29 18:22:24 +1000

    Subsume Utility project into ViewModels

--- Utility/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Resources;
-
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
